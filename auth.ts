d0ad95030477467ba7301fd201e95abb8c07d251
TheSuperior1989 <christiaanvonstade@gmail.com>
2025-11-03 22:03:24 +0200

    Fix: Add trustHost:true to NextAuth config for production

--- auth.ts
+++ auth.ts
@@ -6,6 +6,7 @@ import bcrypt from "bcryptjs"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma) as any,
+  trustHost: true,
   session: {
     strategy: "jwt",
   },
